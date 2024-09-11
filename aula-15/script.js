3dd07abe3474533331a9be9f2a12d867ea906b5a
Brenda Kelly Oliveira Dos Anjos <brenda6078@digitalcollegesul.local>
2024-09-11 19:10:19 -0300

    aula 15

---
+++ aula-15/script.js
@@ -0,0 +1,9 @@
+// METODOS ARRAY
+
+// SLICE: Recorte para impressão de um array (indice inicial e o indice final -1)
+const number = [0, 2, 5, 6, 7, 8]
+console.log(number.slice(1,3));
+
+
+//FROM:
+Array.from(({length: 10})).map(()=>console.log(Array);)
