ab3511204c8bdc0b89dc251f1e2da98f8728a6d2
Brenda Kelly Oliveira Dos Anjos <brenda6078@digitalcollegesul.local>
2024-10-09 20:08:49 -0300

    parte 2 aula

--- aula-20/script.js
+++ aula-20/script.js
@@ -3,9 +3,10 @@ const API_BASE_URL = 'https://services-products.p7fvz0.easypanel.host/api'
 
 async function getProducts(){
   try {
-    const reponse = await fetch(`${API_BASE_URL} /products)`)
-    const data = await reponse.json()
-    console.log(data);
+    const reponse = await fetch(`${API_BASE_URL}/products`)
+    const { results } = await reponse.json()
+    listarProdutos(results)
+    console.log(data.results);
   } catch (error) {
     console.log(error);
     
@@ -14,8 +15,29 @@ async function getProducts(){
 
 getProducts()
 
-async function getProducts() { }
 async function getProductById(id) { }
 async function patchProduct(id) { }
 async function deleteProduct(id) { }
 
+function listarProdutos(produtos) {
+  const listarProdutos= document.getElementById('lista-produtos')
+
+  listarProdutos.innerHTML = " "
+
+  produtos.forEach((produto)=> {
+    listarProdutos.innerHTML += `
+        <tr>
+          <td>${produto.name}</td>
+          <td>${produto.price}</td>
+          <td>${produto.category}</td>
+          <td>
+            <button onclick="deletarProduto(${produto.id})">Remover</button>
+          </td>
+          <td>
+            <button onclick="abrirModal(${produto.id})">Editar</button>
+          </td>
+        </tr>
+    
+    `  
+  });
+}
