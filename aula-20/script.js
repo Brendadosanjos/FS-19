32a0c64a7eb21b7c46e7d2ef057c33524cad616d
Brenda Kelly Oliveira Dos Anjos <brenda6078@digitalcollegesul.local>
2024-10-09 21:40:06 -0300

    aula do dia 09

--- aula-20/script.js
+++ aula-20/script.js
@@ -1,31 +1,73 @@
 const API_BASE_URL = 'https://services-products.p7fvz0.easypanel.host/api'
 
-
-async function getProducts(){
+async function getProducts() {
   try {
-    const reponse = await fetch(`${API_BASE_URL}/products`)
-    const { results } = await reponse.json()
+    const response = await fetch(`${API_BASE_URL}/products?page_size=100`)
+    const { results } = await response.json()
     listarProdutos(results)
-    console.log(data.results);
   } catch (error) {
-    console.log(error);
-    
+    console.error(error.message)
   }
 }
 
 getProducts()
 
+async function postProduct() {
+  const name = document.getElementById('nome')
+  const price = document.getElementById('preco')
+  const category = document.getElementById('categoria')
+
+  const product = {
+    name: name.value,
+    price: price.value,
+    category: category.value
+  }
+
+  try {
+    await fetch(`${API_BASE_URL}/products`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(product)
+    })
+  } catch (error) {
+    console.error(error.message)
+  }
+
+  name.value = ''
+  price.value = ''
+  category.value = ''
+
+  getProducts()
+}
+
 async function getProductById(id) { }
 async function patchProduct(id) { }
 async function deleteProduct(id) { }
 
+// FUNÇÕES DE MANIPULAÇÃO DO DOM
+
+async function deleteProduct(id) {
+  try {
+    await fetch(`${API_BASE_URL}/products/${id}`, {
+      method: 'DELETE'
+    })
+  } catch (error) {
+    console.error(error.message)
+  }
+  getProductBy()
+}
+
+
+
 function listarProdutos(produtos) {
-  const listarProdutos= document.getElementById('lista-produtos')
-
-  listarProdutos.innerHTML = " "
-
-  produtos.forEach((produto)=> {
-    listarProdutos.innerHTML += `
+  const listaProdutos = document.getElementById("lista-produtos")
+  
+  listaProdutos.innerHTML = ""
+  
+  produtos.forEach((produto) => {
+    listaProdutos.innerHTML += `
         <tr>
           <td>${produto.name}</td>
           <td>${produto.price}</td>
@@ -37,7 +79,6 @@ function listarProdutos(produtos) {
             <button onclick="abrirModal(${produto.id})">Editar</button>
           </td>
         </tr>
-    
-    `  
+    `;
   });
 }
