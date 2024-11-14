8e5e1d0f2e04e311e28ae8090dd5d83b46df29ea
unknown <brendaanjos6078@digitalcollege.internal>
2024-11-13 21:49:48 -0300

    estilização

--- aula-25/aula-25/src/components/Card.tsx
+++ aula-25/aula-25/src/components/Card.tsx
@@ -7,11 +7,11 @@ interface CardProps{
 
 export function Card(props: CardProps){
     return(
-        <div className=" bg-gray-200 flex flex-col content-between w-60 border-blue-300 rounded-lg shadow-lg my-5 gap-2 p-3 font-sans mx-3">
+        <div className=" bg-gray-300 flex flex-col content-between w-60 border-blue-500 rounded-lg shadow-lg shadow-slate-800 my-5 gap-2 p-3 font-sans mx-10">
         <h1 className="text-xl">{props.title}</h1>
-        <div>{props.subtitle}</div>
-        <div>{props.content}</div>
-        <hr className="border-gray-300 border"/>
+        <div className="text-gray-700">{props.subtitle}</div>
+        <div className="text-gray-700">{props.content}</div>
+        <hr className="border-gray-500 border"/>
         <div>{props.footer}</div>
         </div>
     )
